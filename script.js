12e0443b43ba7135e453577e003a628af6185c2f
RiccardoOrlando <riccardoorlando08@gmail.com>
2024-11-28 18:11:29 +0100

    feat: add variables

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const numkm = prompt("Quanti km devi percorrere?")
 console.log(numkm)
 const età = prompt("Quanti anni hai?")
 console.log(età)
+const pricebigl = Number(numkm * 0.21)
+// SCONTO UNDER 18
+const discountunder = Number((pricebigl / 100) * 20)
+// PREZZO BIGLIETTO CON SCONTO UNDER 18
+const under = Number(pricebigl - discountunder)
+// PREZZO BIGLIETTO CON DUE UNITà DECIMALI ALLA FINE
+const priceunder = Number(under.toFixed(2))
+// SCONTO OVER 65
+const discountover = Number((pricebigl / 100) * 40)
+// PREZZO BIGLIETTO CON SCONTO OVER 65
+const over = Number(pricebigl - discountover)
+// PREZZO BIGLIETTO CON DUE UNITà DECIMALI ALLA FINE
+const priceover = Number(over.toFixed(2))
