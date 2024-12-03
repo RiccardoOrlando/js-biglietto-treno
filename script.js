965c76351a38878c944138c3cfa08c4bfa0a6fba
RiccardoOrlando <riccardoorlando08@gmail.com>
2024-12-03 17:49:28 +0100

    fix: delete useless "number"

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const numkm = prompt("Quanti km devi percorrere?")
 console.log(numkm)
 const età = prompt("Quanti anni hai?")
 console.log(età)
-const ticket = Number(numkm * 0.21)
+const ticket = (numkm * 0.21)
 // PREZZO BIGLIETTO CON DUE UNITà DECIMALI ALLA FINE
 const  pricebigl= Number(ticket.toFixed(2))
 // SCONTO UNDER 18
-const discountunder = Number((pricebigl / 100) * 20)
+const discountunder = ((pricebigl / 100) * 20)
 // PREZZO BIGLIETTO CON SCONTO UNDER 18
-const under = Number(pricebigl - discountunder)
+const under = (pricebigl - discountunder)
 // PREZZO BIGLIETTO CON DUE UNITà DECIMALI ALLA FINE
 const priceunder = Number(under.toFixed(2))
 // SCONTO OVER 65
-const discountover = Number((pricebigl / 100) * 40)
+const discountover =((pricebigl / 100) * 40)
 // PREZZO BIGLIETTO CON SCONTO OVER 65
-const over = Number(pricebigl - discountover)
+const over = (pricebigl - discountover)
 // PREZZO BIGLIETTO CON DUE UNITà DECIMALI ALLA FINE
 const priceover = Number(over.toFixed(2))
 if (età < 18){
